10af955b2009d7f1571ecd9910c8d50bd2a2d800
kennja05 <jacobkenny05@gmail.com>
2020-03-18 11:28:20 -0400

    added some formatting to trip show page

--- src/components/TripShow.js
+++ src/components/TripShow.js
@@ -1,9 +1,46 @@
 import React from 'react'
+import ReactLoading from 'react-loading'
+import PlannedExpenseForm from './forms/PlannedExpenseForm'
+
 
 export default class TripShow extends React.Component {
 
+    state = {
+        trip: {},
+        loaded: false
+    }
+
+
+    componentDidMount(){
+        fetch('http://localhost:3000/api/v1/trips/11') //the 11 at the end should be obtained from the props
+            .then(res => res.json())
+            .then(trp => this.setState({
+                trip: trp,
+                loaded: true
+            }))
+    }
+
     render(){
-        return(0)
+        return(
+            this.state.loaded ? 
+            <div className='trip-show'>
+                
+                <h1>Your Trip to {this.state.loaded && this.state.trip.destination.name} </h1>
+                <img className='flag-pic' alt='flag' src={this.state.trip.destination.image} />
+                <h2>Start Date: {this.state.trip.start_date}</h2>
+                <h2>End Date: {this.state.trip.start_date}</h2>
+                <h2>Destination Info</h2>
+                <ul>
+                    <li><b>Native Name: </b>{this.state.trip.destination.native_name}</li>
+                    <li><b>Capital: </b> {this.state.trip.destination.capital}</li>
+                    <li><b>Currency Name: </b>{this.state.trip.destination.currency_name} ({this.state.trip.destination.symbol})</li>
+                    <ul><li>Official Currency Code: {this.state.trip.destination.code}</li></ul>
+                </ul>
+
+
+
+            </div> : <ReactLoading type={'spin'} color={'000'} />
+        )
     }
 
 
---
+++ src/components/forms/PlannedExpenseForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+export default class PlannedExpenseForm extends React.Component {
+
+
+    render(){
+        return(
+            0
+        )
+    }
+
+}
