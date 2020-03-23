6264f33da9cb73ff889f56f64f3a2e426e15ece1
kennja05 <jacobkenny05@gmail.com>
2020-03-23 19:58:32 -0400

    added search function to all destinations page

--- src/components/AllDestinations.js
+++ src/components/AllDestinations.js
@@ -6,6 +6,7 @@ export default class AllDestinations extends React.Component {
 
     state = {
         destinations: [],
+        searchTerm: "",
         loaded: false
     }
 
@@ -18,15 +19,27 @@ export default class AllDestinations extends React.Component {
             }))
     }
 
+    handleSearch = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
     render(){
         return( 
             <div>
                 <h2>All Destinations - {this.state.loaded ? `${this.state.destinations.length} total` : <ReactLoading type={'spin'} color={'000'}/>} </h2>
                 {this.props.user ? <Link className='link' to='/dashboard'>
                     <p>Return to Dashboard</p>
-                </Link> : <Link className='link' to='/'>Log In / Sign Up</Link>}
+                </Link> : <Link className='link' to='/'>Log In / Sign Up</Link>}<br></br>
+                
+                {this.state.loaded && <div>
+                    <p><u>Search for a Country: </u>
+                    <input onChange={this.handleSearch} type='text' value={this.state.searchTerm} name='searchTerm'/></p>
+                </div>}
+                
                 <ul>
-                    {this.state.destinations.map(destination => <li key={destination.id}>
+                    {this.state.destinations.filter(dest => dest.name.includes(this.state.searchTerm.toUpperCase())).map(destination => <li className='all-countries-li' key={destination.id}>
                         <b>{destination.name}</b> (Local Name: {destination.native_name})
                             <ul>
                                 <li><u>Capital</u>: {destination.capital}</li>
