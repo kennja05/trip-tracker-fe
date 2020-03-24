11e4e0fde2d36cdb8b4caf331656020b97c083b8
kennja05 <jacobkenny05@gmail.com>
2020-03-24 16:11:42 -0400

    added styling to credential page and homepage container

--- src/components/Credentials.js
+++ src/components/Credentials.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import LoginForm from './forms/LoginForm'
 import SignupForm from './forms/SignupForm'
+import Title from './Title'
 
 export default class Credentials extends React.Component {
     render(){
         return(
             <div className='credential-page'>
-                <h1 className='title'>TRI₽TRA€Kr</h1>
                 <LoginForm handleLogin={this.props.handleLogin} routerProps={this.props} />
+                <Title />
                 <SignupForm handleSignup={this.props.handleSignup} routerProps={this.props}/>
             </div>
         )
--- src/components/HomepageContainer.js
+++ src/components/HomepageContainer.js
@@ -1,21 +1,21 @@
 import React from 'react'
-// import CurrencyContainer from './CurrencyContainer'
+import CurrencyContainer from './CurrencyContainer'
 import PopularDestinationsContainer from './PopularDestinationsContainer'
 import TripContainer from './TripContainer'
 import AddTripForm from './forms/AddTripForm'
-
+import NavBar from './NavBar'
 
 
 const HomepageContainer = (props) => {
 
     return(
-        <div className='Homepage'>
-        {props.user ? <button onClick={() => props.history.push('/')}>LOGOUT</button> : null}
         <div>
-            {/* <CurrencyContainer user={props.user}/> */}
+            <NavBar />
+        <div className='Homepage'>
             <PopularDestinationsContainer />
-            <TripContainer user={props.user} history={props.history} match={props.match}/>
+            <CurrencyContainer user={props.user}/>
             <AddTripForm user={props.user} history={props.history} match={props.match}/>
+            <TripContainer user={props.user} history={props.history} match={props.match}/>
         </div>
         </div>
 
