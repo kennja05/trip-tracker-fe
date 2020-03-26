5db7e15e8572b45bfc8a1b401d509fd19bf19341
kennja05 <jacobkenny05@gmail.com>
2020-03-26 14:59:57 -0400

    fixed bug with historical rates

--- src/components/HistoricalRates.js
+++ src/components/HistoricalRates.js
@@ -14,7 +14,8 @@ export default class HistoricalRates extends React.Component {
     }
 
     getFirstAndLastRates = (valueList, datePlanned) => {
-        const fv = valueList.filter(val => val.date === datePlanned)[0].rate
+        const myvals = valueList.filter(val => val.date === datePlanned)
+        const fv = myvals[myvals.length-1].rate
         this.setState({
             startRate: fv,
             currentRate: valueList[valueList.length-1].rate
@@ -31,11 +32,11 @@ export default class HistoricalRates extends React.Component {
                         {/* Get only the values from after the trip was created, then create the list */}
                         {this.props.values.filter(val => val.date >= this.props.startDate).map(vals => <li key={vals.id}>Date:{vals.date} Exchange Rate: {vals.rate} {vals.code} : 1 USD</li>)}
                     </ul>
-                    A trip costing 10,000 ${this.props.destination.currency_name} would cost ${(10000 / this.state.startRate).toFixed(2)} United States Dollars if the currency were exchanged at the time this trip was entered to the site. The same trip would cost ${(10000 / this.state.currentRate).toFixed(2)} if the currency were exchanged now.`
+                    <p>A trip costing 10,000 {this.props.destination.currency_name} would cost ${(10000 / this.state.startRate).toFixed(2)} United States Dollars if the currency were exchanged at the time this trip was entered to the site. The same trip would cost ${(10000 / this.state.currentRate).toFixed(2)} if the currency were exchanged now.</p>
                 </div>
             </div>
         )
     }
-
+ 
 
 }
