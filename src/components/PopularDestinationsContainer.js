1135796d712155d90021e25b3401aae378f63a33
kennja05 <jacobkenny05@gmail.com>
2020-03-26 14:14:44 -0400

    css for homepage layout

--- src/components/PopularDestinationsContainer.js
+++ src/components/PopularDestinationsContainer.js
@@ -22,7 +22,7 @@ class PopularDestinationsContainer extends React.Component {
         return(
             <div id='popular-destinations-container' className='Dashboard-Container'>
                 <div className='sub-dash-container-div'>
-                    <h2><u>Top 15 Destinations</u></h2>{this.state.loaded ? null : <ReactLoading type={'spin'} color={'#6b6e70'}/>}
+                    <h2><u>Top Destinations</u></h2>{this.state.loaded ? null : <ReactLoading type={'spin'} color={'#6b6e70'}/>}
                     <ol>
                     {this.state.topDestinations.map(dest => <li key={dest.id}>{dest.name} - {dest.trips.length} users have gone here</li>)}  
                     </ol>
