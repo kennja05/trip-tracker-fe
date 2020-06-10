d89639b2037c1bb9c46ea2c673cb25094012da10
kennja05 <jacobkenny05@gmail.com>
2020-06-10 18:29:34 -0400

    grouped css elements together by page

--- src/components/HomepageContainer.js
+++ src/components/HomepageContainer.js
@@ -10,19 +10,18 @@ class HomepageContainer extends React.Component {
    
     render(){
         return(
-          <div>
-              <NavBar user={this.props.user} logout={this.props.logout} history={this.props.history}/>
-              <div className='Homepage'>
-                  <div className='popular-add-trip'>
-                      <PopularDestinationsContainer />
-                      <AddTripForm user={this.props.user} history={this.props.history} match={this.props.match}/>
-                  </div>
-                  <div className='present-past-trips'>
-                      <TripContainer user={this.props.user} history={this.props.history} match={this.props.match}/>
-                      <PastTrips user={this.props.user} history={this.props.history} match={this.props.match}/>
-                  </div>
-              </div>
-          </div>
+        <div>
+            <div className='Homepage'>
+                <div className='popular-add-trip'>
+                    <PopularDestinationsContainer />
+                    <AddTripForm user={this.props.user} history={this.props.history} match={this.props.match}/>
+                </div>
+                <div className='present-past-trips'>
+                    <TripContainer user={this.props.user} history={this.props.history} match={this.props.match}/>
+                    <PastTrips user={this.props.user} history={this.props.history} match={this.props.match}/>
+                </div>
+            </div>
+        </div>
         )
     }
 
