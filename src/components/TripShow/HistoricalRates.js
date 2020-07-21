9a2c7f3255da1274cd223fc2691357011115fe4e
kennja05 <jacobkenny05@gmail.com>
2020-07-21 17:31:08 -0400

    add momentjs for dateformatting on historical rates component

--- src/components/TripShow/HistoricalRates.js
+++ src/components/TripShow/HistoricalRates.js
@@ -1,45 +1,24 @@
 import React from 'react'
+import Moment from 'react-moment'
 
 export default class HistoricalRates extends React.Component {
 
-
-    // state={
-    //     startRate: null,
-    //     currentRate: null
-    // }
-
-    componentDidMount(){
-        const {rates} = this.props
-        const rateArray = []
-    }
-
-    // getFirstAndLastRates = (valueList, datePlanned) => {
-    //     const myvals = valueList.filter(val => val.date === datePlanned)
-    //     const fv = myvals[myvals.length-1].rate
-    //     this.setState({
-    //         startRate: fv,
-    //         currentRate: valueList[valueList.length-1].rate
-    //     })
-    // }
-
-    formatDate = dateStr => { //will format a date string that looks like '2020-07-21T14:17:21.717Z' 
-        let arr = dateStr.split('T')
-    }
-
     render(){
+        console.log(this.props)
         return(
             <div className='historical-rates'>
                 <div className='hr-info'>
                     <h2 className='pe-h2'>This trip was planned on {this.props.startDate}</h2>
                     <p>Rate Changes Since then:</p>
                     <ul>
-                        {this.props.rates.map((vals,i) => <li key={i}>Date: {vals.date} || 
-                        Exchange Rate: {vals.rate} {vals.code} : 1 USD</li>)}
+                        {this.props.rates.map((vals,i) => <li key={i}>Date:  
+                        <Moment format='YYYY-MM-DD hh:mm a'>{vals.date}</Moment>
+                            || 
+                        Exchange Rate: {vals.rate} {this.props.destination.code} : 1 USD</li>)}
                     </ul>
                 </div>
             </div>
         )
     }
  
-
 }
