02085e2f8fd8574c8d1ec3b65b97adbbd6c87a47
kennja05 <jacobkenny05@gmail.com>
2020-07-21 20:27:09 -0400

    clean up historical rates component

--- src/components/TripShow/HistoricalRates.js
+++ src/components/TripShow/HistoricalRates.js
@@ -4,17 +4,18 @@ import Moment from 'react-moment'
 export default class HistoricalRates extends React.Component {
 
     render(){
-        console.log(this.props)
+        const {destination, rates, startDate} = this.props
         return(
             <div className='historical-rates'>
                 <div className='hr-info'>
-                    <h2 className='pe-h2'>This trip was planned on {this.props.startDate}</h2>
-                    <p>Rate Changes Since then:</p>
+                    <h2 className='pe-h2'>This trip was planned on {startDate}</h2>
+                    <p>Rates Since then:</p>
                     <ul>
-                        {this.props.rates.map((vals,i) => <li key={i}>Date:  
-                        <Moment format='YYYY-MM-DD hh:mm a'>{vals.date}</Moment>
-                            || 
-                        Exchange Rate: {vals.rate} {this.props.destination.code} : 1 USD</li>)}
+                        {rates.map((vals,i) => <li key={i}> 
+                        {destination.symbol}{vals.rate} {destination.code} : 
+                        $1 USD 
+                        (<Moment date={vals.date} format='MM-DD-YYYY hh:mm a'/>) 
+                        </li>)}
                     </ul>
                 </div>
             </div>
