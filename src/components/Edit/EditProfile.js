e95ddb2f40b267bbdfb18a22e47dbd55b0842181
kennja05 <jacobkenny05@gmail.com>
2020-08-25 16:37:25 -0700

    add link to profile page in navbar

--- src/components/Edit/EditProfile.js
+++ src/components/Edit/EditProfile.js
@@ -3,11 +3,12 @@ import React from 'react'
 export default class EditProfile extends React.Component {
 
     render(){
+        const {user} = this.props
+        console.log(user)
         return(
             <div id='edit-profile-page'>
                 <div id='edit-form'>
-                Edit ur profile hereeee...
-
+        <h1>{this.props.user ? user.username : "Log in son"}</h1>
                 </div>
             </div>
         )
