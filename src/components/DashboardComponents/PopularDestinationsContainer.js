1a71cf7019429fc32b77d436b1667f83b3ab8026
kennja05 <jacobkenny05@gmail.com>
2020-10-07 18:58:34 -0400

    animate popular destinations container

--- src/components/DashboardComponents/PopularDestinationsContainer.js
+++ src/components/DashboardComponents/PopularDestinationsContainer.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactLoading from 'react-loading' 
 import { Link } from 'react-router-dom'
+import styled, { keyframes } from 'styled-components'
+import {slideInLeft} from 'react-animations'
 
 class PopularDestinationsContainer extends React.Component {
 
@@ -19,20 +21,29 @@ class PopularDestinationsContainer extends React.Component {
     }
  
     render(){
+
+        const fadeAnimation = keyframes`${slideInLeft}`
+        const StyledDiv = styled.div`
+            animation: 1s ${fadeAnimation}
+        `
+
         const {loaded, topDestinations} = this.state
         return(
+            
             <div id='popular-destinations' className='dashboard-container'>
-                <div className='sub-dash-container-div'>
-                    <h2 style={{fontFamily: 'Racing Sans One'}}><u>Top Destinations by Entire Site</u></h2>
-                    {loaded ? null : <ReactLoading type={'spin'} color={'#6b6e70'}/>}
-                    <ol>
-                        {topDestinations.map(dest => 
-                            <li key={dest.id}>
-                                {dest.name} - {dest.trips.length} Trips
-                            </li>)}  
-                    </ol>
-                    <Link className='link' to='/alldestinations'>See All</Link>
-                </div>
+                <StyledDiv>
+                    <div className='sub-dash-container-div'>
+                        <h2 style={{fontFamily: 'Racing Sans One'}}><u>Top Destinations by Entire Site</u></h2>
+                        {loaded ? null : <ReactLoading type={'spin'} color={'#6b6e70'}/>}
+                        <ol>
+                            {topDestinations.map(dest => 
+                                <li key={dest.id}>
+                                    {dest.name} - {dest.trips.length} Trips
+                                </li>)}  
+                        </ol>
+                        <Link className='link' to='/alldestinations'>See All</Link>
+                    </div>
+                </StyledDiv>
             </div>
         )
     }
